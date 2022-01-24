321f48380a3c30eaa84da2a9f410e44eaafbe5df
madison sorah <madisonsorah@gmail.com>
2022-01-24 10:50:12 -0500

    Installed React Router

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # route to test your configuration
   get '/hello', to: 'application#hello_world'
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
