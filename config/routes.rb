572c667f8334c4ad62ad99fab5b70d8f6c541deb
madison sorah <madisonsorah@gmail.com>
2022-01-24 12:07:49 -0500

    Removed controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # route to test your configuration
   get '/hello', to: 'application#hello_world'
-
-  get '*path',
-      to: 'fallback#index',
-      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
