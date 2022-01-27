8e797c7729d1e1cb7ffd3bc81b0f944793fc4db0
madison sorah <madisonsorah@gmail.com>
2022-01-27 11:35:06 -0500

    Tweaked routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
 
   resources :members, only: [:index, :show, :create, :update, :destroy]
-  resources :playlistrequests, only: [:index, :show, :create, :update, :destroy]
-  resources :createdplaylists, only: [:index, :show, :update, :destroy]
+  resources :requested_playlists, only: [:index, :show, :create, :update, :destroy]
+  resources :created_playlists, only: [:index, :show, :update, :destroy]
 
   get '*path',
   to: 'fallback#index',
