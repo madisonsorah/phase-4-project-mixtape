22776eb6bf7f0d4f6488065c42f95fbe002a061a
Dayton <kirylmitch@gmail.com>
2022-01-28 12:49:47 -0500

    added signup form, altered css

--- client/src/Components/SignupPage.js
+++ client/src/Components/SignupPage.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Form } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 
 function SignupPage () {
     return (
-        <>
-        <h1>Sign Up</h1>
-        
-        </>
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Enter your email</Form.Label>
+          <Form.Control type="email" placeholder="Email" />
+          <Form.Text className="text-muted">
+            
+          </Form.Text>
+        </Form.Group>
+      
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Create a Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Continue
+        </Button>
+      </Form>
         
     )
 }
